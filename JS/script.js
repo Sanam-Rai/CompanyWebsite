f8c4ee6492e72f514416131777f31b76a15d2e02
Sanam-Rai <58116573+Sanam-Rai@users.noreply.github.com>
2019-12-17 18:59:13 -0400

    update on CSS file for the sticky header margin-top property

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,4 @@
-/* implementing sticky header */
+//Sticky header with the scroll anitmation 
 
-Window.onscroll = function() {myFuncton()};
-//get the header
-var header= document.getElementsByTagName("header");
-// offset position of the nav bar
-var sticky= header.offsetTop;
 
-//Add the sticky class to the header when it is scroll position
-function myFuncton(){
-    if( window.pageYOffset > sticky){
-        header.classList.add("sticky");
-    }
-    else{
-        header.classList.remove("sticky");
-    }
-}
+// add sticky 
